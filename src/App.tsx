79e112803b164ecf38b9142b447e49a786ef526a
JJoseph1337 <dassdass733@gmail.com>
2024-02-15 18:09:07 +0500

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Header from "./components/header/Header";
+import styles from "./App.module.css";
+
 function App() {
-  return <></>;
+  return (
+    <div className={styles.layout}>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
