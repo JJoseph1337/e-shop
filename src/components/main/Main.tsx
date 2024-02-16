0e55115b9811ab8fa0a687be1fb996472f98647e
JJoseph1337 <dassdass733@gmail.com>
2024-02-16 09:38:30 +0500

    fix svg icon

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,15 @@
+const Main = () => {
+  return <div>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Account</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+
+    <main></main>
+  </div>;
+};
+
+export default Main;
