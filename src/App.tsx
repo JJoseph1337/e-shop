b3f66e2d222d5e5a98026b8c4a4884f5ed36c2e2
JJoseph1337 <dassdass733@gmail.com>
2024-02-16 10:13:57 +0500

    Add main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.layout}>
       <Header />
+      <Main />
     </div>
   );
 }
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,15 +1,41 @@
-const Main = () => {
-  return <div>
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>Account</li>
-        <li>Login</li>
-      </ul>
-    </nav>
+import styles from "./Main.module.css";
 
-    <main></main>
-  </div>;
+const Main = () => {
+  return (
+    <div className={styles.container}>
+      <nav className={styles.nav}>
+        <ul>
+          <li>Home</li>
+          <li>Account</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+
+      <main className={styles.main}>
+        <article>
+          <h2>Title</h2>
+          <img
+            src="https://via.placeholder.com/600/771796"
+            alt="image"
+          />
+        </article>
+        <article>
+          <h2>Title</h2>
+          <img
+            src="https://via.placeholder.com/600/d32776"
+            alt="image"
+          />
+        </article>
+        <article>
+          <h2>Title</h2>
+          <img
+            src="https://via.placeholder.com/600/f66b97"
+            alt="image"
+          />
+        </article>
+      </main>
+    </div>
+  );
 };
 
 export default Main;
